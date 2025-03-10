d0c03e3db90810ef0c18461329786b0e276be4e9
OhhRayyd <rayyd.inc@gmail.com>
2025-03-09 21:56:28 -0400

    AddBinary - FirstOccurrenceString leetcode

---
+++ LeetCode/src/main/java/String/AddBinary.java
@@ -0,0 +1,33 @@
+package String;
+
+public class AddBinary {
+
+    public static void main(String[] args) {
+        String a = "11";
+        String b = "1";
+        System.out.println(addBinary(a, b));
+
+    }
+
+    public static String addBinary(String a, String b) {
+        int firstPointer = a.length() - 1;
+        int secondPointer = b.length() - 1;
+        int carry = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        while (firstPointer >= 0 || secondPointer >= 0 || carry > 0) {
+            int bit1 = firstPointer >= 0 ? a.charAt(firstPointer) - '0' : 0;
+            int bit2 = secondPointer >= 0 ? b.charAt(secondPointer) - '0' : 0;
+
+            int sum = bit1 + bit2 + carry;
+            carry = sum / 2;
+            sb.append(sum % 2);
+
+            firstPointer--;
+            secondPointer--;
+        }
+
+        return sb.reverse().toString();
+    }
+}
---
+++ LeetCode/src/main/java/String/FirstOccurrenceString.java
@@ -0,0 +1,20 @@
+package String;
+
+public class FirstOccurrenceString {
+
+    public static void main(String[] args) {
+        String haystack = "Applerock";
+        String needle = "lero";
+        System.out.println(strStr(haystack, needle));
+    }
+
+
+    public static int strStr(String haystack, String needle){
+
+        if(!haystack.contains(needle)){
+            return -1;
+        } else {
+            return haystack.indexOf(needle);
+        }
+    }
+}
